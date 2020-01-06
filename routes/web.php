ab4aeb1433840b2dcab218b8c5ad9ed4bacff38a
Gianluca Bine <gian_bine@hotmail.com>
2020-01-06 11:55:36 -0300

    Scaffold de autenticação

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
