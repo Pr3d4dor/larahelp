41cf4a5e1211183a8a59f12b703de14e4921997c
Gianluca Bine <gian_bine@hotmail.com>
2020-01-06 13:43:04 -0300

    Implemendo testes de controller de login

---
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use App\Models\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function login_displays_the_login_form()
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
+    }
+
+    /** @test */
+    public function login_displays_validation_errors()
+    {
+        $response = $this->post('/login', []);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function login_authenticates_and_redirects_user()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $response->assertRedirect(route('admin.dashboard'));
+        $this->assertAuthenticatedAs($user);
+    }
+}
