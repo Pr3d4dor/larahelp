e08563d5b1ff26dd345b35de7cf03a97b31b1294
Gianluca Bine <gian_bine@hotmail.com>
2020-01-10 14:17:03 -0300

    Adequação para aplicar select2 automaticamente quando a classse select2 estiver presente e adicionado função de gerar slug baseado em texto

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -49,6 +49,28 @@
                 "url": '{{ asset('vendor/datatables/js/Portuguese.json') }}'
             },
         });
+
+        // Aplicar select2
+        $('.select2').select2();
+
+        window.slugify = function slugify(str) {
+            str = str.replace(/^\s+|\s+$/g, ''); // trim
+            str = str.toLowerCase();
+
+            // remove accents, swap ñ for n, etc
+            var from = "àáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
+            var to   = "aaaaaeeeeiiiioooouuuunc------";
+
+            for (var i=0, l=from.length ; i<l ; i++) {
+                str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+            }
+
+            str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+                .replace(/\s+/g, '-') // collapse whitespace and replace by -
+                .replace(/-+/g, '-'); // collapse dashes
+
+            return str;
+        }
     });
 </script>
 
