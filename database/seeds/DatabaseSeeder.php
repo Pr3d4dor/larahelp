1c605393eafb9f6df666aba37eb2ece2f8ad8ac2
Gianluca Bine <gian_bine@hotmail.com>
2020-01-12 14:20:17 -0300

    Adequação para executar seeder de perguntas frequentes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             TagTableSeeder::class,
             ArticleTableSeeder::class,
             FaqCategoriesTableSeeder::class,
+            FaqQuestionTableSeeder::class,
         ]);
     }
 }
