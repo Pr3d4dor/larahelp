de08a74add43eca0e036207f26b2811dec4d09d7
Gianluca Bine <gian_bine@hotmail.com>
2020-01-19 16:14:12 -0300

    Refatoração de código

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -42,5 +42,9 @@ class AppServiceProvider extends ServiceProvider
         });
 
         Schema::defaultStringLength(191);
+
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
