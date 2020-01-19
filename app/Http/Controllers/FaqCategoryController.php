3533b2f93aa077e30cbdc3048a29962b5c183ec6
Gianluca Bine <gian_bine@hotmail.com>
2020-01-19 17:01:18 -0300

    Correção de erro com where em função agregada

--- app/Http/Controllers/FaqCategoryController.php
+++ app/Http/Controllers/FaqCategoryController.php
@@ -8,10 +8,9 @@ class FaqCategoryController extends Controller
 {
     public function index()
     {
-        $faqCategories = FaqCategory::withCount('faqQuestions')
+        $faqCategories = FaqCategory::has('faqQuestions', '>=', 1)
+            ->withCount('faqQuestions')
             ->with('faqQuestions')
-            ->where('faq_questions_count', '>', 0)
-            ->orderBy('id')
             ->get();
 
         return view('faq_categories.index', compact('faqCategories'));
