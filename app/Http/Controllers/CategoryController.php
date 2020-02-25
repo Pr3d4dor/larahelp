1c215a856f035bdf579031f0733e3d153bcc89d5
Gianluca Bine <gian_bine@hotmail.com>
2020-02-25 15:57:41 -0300

    Implementado listagem de categorias de artigos

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,9 +5,26 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
+    public function index()
+    {
+        $categories = Category::select([
+                'categories.*',
+                'articles.view_count',
+                DB::raw('SUM(articles.view_count) as total')
+            ])
+            ->leftJoin('articles', 'categories.id', '=', 'articles.category_id')
+            ->withCount('articles')
+            ->groupBy('categories.id')
+            ->orderBy('total', 'DESC')
+            ->paginate(10);
+
+        return view('categories.index', compact('categories'));
+    }
+
     public function show(Request $request, Category $category)
     {
         $articles = $category
