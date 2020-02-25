2a3daa0a2031dd555c71661638b20103bf181180
Gianluca Bine <gian_bine@hotmail.com>
2020-02-25 15:57:52 -0300

    Implementado listagem de tags de artigos

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -5,9 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TagController extends Controller
 {
+    public function index()
+    {
+        $tags = Tag::select([
+            'tags.*',
+            'articles.view_count',
+            DB::raw('SUM(articles.view_count) as total')
+        ])
+            ->leftJoin('article_tag', 'article_tag.tag_id', '=', 'tags.id')
+            ->leftJoin('articles', 'article_tag.article_id', '=', 'articles.id')
+            ->withCount('articles')
+            ->groupBy('tags.id')
+            ->orderBy('total', 'DESC')
+            ->paginate(10);
+
+        return view('tags.index', compact('tags'));
+    }
+
     public function show(Request $request, Tag $tag)
     {
         $articles = $tag
